9f5c16d9d36655bcb71cf481c723a587ec7ee2a5
JTWoodson <jingtianli.animation@gmail.com>
2021-07-04 20:05:59 -0500

    serilized all varaibles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [SerializeField] float xValue = 0.01f;
-    float yValue = 0.01f;
-    float zValue = 0.01f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0.01f;
 
     // Start is called before the first frame update
     void Start()
